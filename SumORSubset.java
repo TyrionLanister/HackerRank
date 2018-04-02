5b0e7f0402443166e616846adcb975bdc2f64513
TyrionLanister <dhiraj@kth.se>
2018-04-02 11:29:17 +0530

    Update SumORSubset.java

--- SumORSubset.java
+++ SumORSubset.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public class SumORSubset{
-    static final int INT_SIZE =32;
+    static final int BIT_SIZE =32;
     static int getBit(int n, int k){
         return (n & (1<<k)) != 0 ? 1 : 0;
     }
     static int bitWiseOR(  int [] arr ){
         int result = 0;
         int n = arr.length;
-        Vector<Integer> bitFreq = new Vector<Integer>(INT_SIZE);
+        Vector<Integer> bitFreq = new Vector<Integer>(BIT_SIZE);
         for(int i = 0;i<INT_SIZE;i++){
             bitFreq.add(0);
         } 
         
         for(int i = 0;i<n;i++){
-            for (int k = INT_SIZE-1; k>=0; k--){
+            for (int k = BIT_SIZE-1; k>=0; k--){
                 bitFreq.set(k,bitFreq.get(k)+getBit(arr[i],k));
             }
             
          }
-        for (int i=0; i < INT_SIZE; ++i){
+        for (int i=0; i < BIT_SIZE; ++i){
             int count = bitFreq.get(i);
             while(count>0){
                 result+= Math.pow(2,i)*Math.pow(2,n-count--);
