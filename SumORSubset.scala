f4f0af0cb1c0fad6b0c2fd557406f0f002f3885f
TyrionLanister <dhiraj@kth.se>
2018-04-02 11:30:25 +0530

    Update SumORSubset.scala

--- SumORSubset.scala
+++ SumORSubset.scala
@@ -1,20 +1,20 @@
 import java.util.Arrays
 object SumORSubset extends App{
-    val INT_SIZE = 32
+    val BIT_SIZE = 32
     def getBit(n: Int, k: Int): Int = {
          val bit = if ((n & (1<<k)) !=0)  1 else 0
          bit
      }
     def bitWiseOR(arr: Array[Int]):Int ={
-        val bitFreq = new Array [Int] (INT_SIZE)
+        val bitFreq = new Array [Int] (BIT_SIZE)
         val n = arr.length
         var result = 0.0
         for(i <- 0 until n){
-            for(k <-INT_SIZE -1 to 0 by -1){
+            for(k <-BIT_SIZE -1 to 0 by -1){
                 bitFreq(k) += getBit(arr(i), k)
             }
         }
-         for(i <- 0 until INT_SIZE){
+         for(i <- 0 until BIT_SIZE){
            for(count <- bitFreq(i) until 0 by -1){
                 result+= Math.pow(2,i)*Math.pow(2,n-count)
             }
