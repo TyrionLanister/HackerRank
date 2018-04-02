cb41686f954f5086198b4b7450d8ae29253b37cf
TyrionLanister <dhiraj@kth.se>
2018-04-02 11:28:41 +0530

    Update SumORSubset.cpp

--- SumORSubset.cpp
+++ SumORSubset.cpp
@@ -2,20 +2,20 @@
 #include <vector>
 #include <cmath>
 using namespace std; 
-#define INT_SIZE 32
+#define BIT_SIZE 32
 #define getBit(n,k) ({ \
     (n & (1<<k)) != 0;\
 })
 int bitWiseOR(vector<int> &arr)
 {
     int result = 0;
-    vector<int> bitFreq(INT_SIZE);
+    vector<int> bitFreq(BIT_SIZE);
     int n = arr.size();
     for(int i = 0;i<arr.size();i++){
-        for (int k = INT_SIZE-1; k>=0; k--)
+        for (int k = BIT_SIZE-1; k>=0; k--)
             bitFreq[k]+= getBit(arr[i],k);
     }
-    for (int i=0; i < INT_SIZE; ++i){
+    for (int i=0; i < BIT_SIZE; ++i){
         while(bitFreq[i]){
             result+= pow(2,i)*pow(2,n-bitFreq[i]--);
         }
